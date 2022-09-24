2b96b2e63ba08a776715db5c8ba22fbe70db7239
Shashvat <thelearningcoder2000@gmail.com>
2022-09-24 12:31:41 +0530

    admin fields

--- Online_Term_Insurance/src/main/java/com/capg/entity/Admin.java
+++ Online_Term_Insurance/src/main/java/com/capg/entity/Admin.java
@@ -1,4 +1,14 @@
 package com.capg.entity;
+
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Entity @Data
 public class Admin {
  
+	
+	private String email;
+	private String password;
+	
 }
